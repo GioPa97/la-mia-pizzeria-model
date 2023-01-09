55dd6227889053d0484cdae6cd0e95c7dc810421
GioPa97 <giovannipagliara97@.com>
2023-01-09 17:33:32 +0100

    Creati elementi nella lista, problema con le immagini

---
+++ La Mia Pizzeria 1/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿using La_Mia_Pizzeria_1.Models;
+using La_Mia_Pizzeria_1.Models.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria_1.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Pizza> listaDellePizza = PizzaData.GetPizzas();
+
+            return View("Index", listaDellePizza);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> listaDellePizza = PizzaData.GetPizzas();
+
+            foreach (Pizza pizza in listaDellePizza)
+            {
+                if (pizza.Id == id)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("la pizza con l'id cercato non esiste!");
+        }
+
+        public IActionResult Esempio(string nome, string cognome, int eta)
+        {
+            return Ok("Hai inserito parametro nome: " + nome + " parametro cognome: " + cognome + " parametro eta: " + eta);
+        }
+    }
+}
---
+++ La Mia Pizzeria 1/Models/Utils/PizzaData.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace La_Mia_Pizzeria_1.Models.Utils
+{
+
+
+    public static class PizzaData
+    {
+        private static List<Pizza>? pizzas;
+
+        public static List<Pizza> GetPizzas()
+        {
+
+            if (pizzas != null)
+            {
+                return pizzas;
+            }
+
+            pizzas = new List<Pizza>();
+
+            Pizza pizza01 = new Pizza(0, "Margherita", "Salsa di pomodoro, mozzarella, basilico fresco", "" + "/100");
+            pizzas.Add(pizza01);
+
+            Pizza pizza02 = new Pizza(1, "Diavola", "Salsa di pomodoro, mozzarella, salame piccante", "https://www.lennesimoblogdicucina.com/wp-content/uploads/2020/06/pizza-margherita-con-fornetto-innerimageoptim-1050x1575.jpg" + "/100");
+            pizzas.Add(pizza02);
+
+            Pizza pizza03 = new Pizza(2, "Prosciutto e funghi", "Salsa di pomodoro, mozzarella, prosciutto cotto, funhi di stagione", "https://picsum.photos/id/1" + "/100");
+            pizzas.Add(pizza03);
+
+            Pizza pizza04 = new Pizza(3, "Marinara", "Salsa di pomodoro fresco, origano, olio extravergine d'oliva", "https://picsum.photos/id/1" + "/100");
+            pizzas.Add(pizza04);
+
+            Pizza pizza05 = new Pizza(4, "Straciatella e speck", "Salsa di pomodoro, straciatella di burrata, speck trentino", "https://picsum.photos/id/1" + "/100");
+            pizzas.Add(pizza05);
+
+            Pizza pizza06 = new Pizza(5, "Bufala", "Salsa di pomodoro, mozzarella di bufala campana, basilico fresco, olio extravergine d'oliva", "https://picsum.photos/id/1" + "/100");
+            pizzas.Add(pizza06);
+
+
+
+
+
+
+            return pizzas;
+        }
+        
+    }
+}
+
